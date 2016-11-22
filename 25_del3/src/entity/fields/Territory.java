8d9f7756b43138790347e1ccb356a63b72ea328e
Frederik <Frederik@-.clients.wireless.dtu.dk>
2016-11-22 11:21:48 +0100

    Rent metoder til flere klasser

--- 25_del3/src/entity/fields/Territory.java
+++ 25_del3/src/entity/fields/Territory.java
@@ -1,15 +1,24 @@
 package entity.fields;
-import entity.*;
+
+import entity.Player;
 
 public class Territory extends Ownable {
-private int rent;
+	private int rent;
 
-public Territory(String fieldname, int fieldnumber, int price,int rent){
-	super(fieldname,fieldnumber,price);
-	this.rent=rent;
-}	
+	public Territory(String fieldname, int fieldnumber, int price, int rent) {
+		super(fieldname, fieldnumber, price);
+		this.rent = rent;
+	}
 
-public int getRent(){
-	return rent;
-}
+	@Override
+	public int getRent() {
+		return rent;
+	}
+
+	private void transferAmount(int amount, Player lander) {
+		int amountWithdraw = lander.getAccount().withdraw(getRent());
+		owner.getAccount().deposit(amountWithdraw);
+		// GUI besked der viser mængden af penge der er blevet flyttet
+
+	}
 }
